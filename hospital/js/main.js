643a811bc00b558dcc190c3d4af64a12118231de
tino <email>
2023-05-13 12:02:31 +0200

    terminado el ejercicio de filtrar paciente por diagnostico

--- hospital/js/main.js
+++ hospital/js/main.js
@@ -1,37 +1,64 @@
 const articleLista = document.querySelector('#listado');
 const ulPacientes = document.querySelector('#ullistado');
 
-function printAllpacients (pLista,pDom){
-    pLista.forEach(paciente => printOnePacient(paciente,pDom));
+function printAllpacients(pLista, pDom) {
+    pLista.forEach(paciente => printOnePacient(paciente, pDom));
 }
 
-function printOnePacient (pacient,pDom){
-pDom.innerHTML+= `<li>"${pacient.nombre}"</li><li>"${pacient.apellidos}"</li><li>"${pacient.edad}"</li><li>"${pacient.diagnostico}"</li><li>"${pacient.seguridadSocial}"</li><hr>`
+function printOnePacient(pacient, pDom) {
+    pDom.innerHTML += `<li>"${pacient.nombre}"</li><li>"${pacient.apellidos}"</li><li>"${pacient.edad}"</li><li>"${pacient.diagnostico}"</li><li>"${pacient.seguridadSocial}"</li><hr>`
 }
 
-printAllpacients(pacientes,ulPacientes);
+printAllpacients(pacientes, ulPacientes);
 
 const edadMin = document.querySelector('#min');
 const edadMax = document.querySelector('#max');
 const btnEdad = document.querySelector('#btnEdad');
 
-btnEdad.addEventListener('click',filtrarEdad);
 
-function filtrarEdad () {
+btnEdad.addEventListener('click', filtrarEdad);
+
+function filtrarEdad() {
 
     ulPacientes.innerHTML = " ";
-    
+
     let max = edadMax.value;
     let min = edadMin.value;
 
     if (max !== " " && min !== " ") {
-        let pacientesPorEdad = filterByAge(pacientes,Number(min),Number(max));
-        printAllpacients(pacientesPorEdad,ulPacientes);
+        let pacientesPorEdad = filterByAge(pacientes, Number(min), Number(max));
+        printAllpacients(pacientesPorEdad, ulPacientes);
     } else {
-        printAllpacients(pacientes,ulPacientes);
+        printAllpacients(pacientes, ulPacientes);
     }
 }
 
-function filterByAge (list,pMin,pMax) {
-    return list.filter(paciente => paciente.edad >=pMin && paciente.edad <= pMax);
+function filterByAge(list, pMin, pMax) {
+    return list.filter(paciente => paciente.edad >= pMin && paciente.edad <= pMax);
+}
+
+const diagnostico = document.querySelector('#diagnostico');
+
+diagnostico.addEventListener('change', filtrarDiagnostico);
+
+function filtrarDiagnostico(event) {
+
+    ulPacientes.innerHTML=" ";
+
+    let diagnosticoFiltrado = event.target.value;
+
+    if (diagnosticoFiltrado !== " ") {
+        let pacientesPorDiagnostico = filtrarPorDiagnostico(pacientes, diagnosticoFiltrado);
+
+        printAllpacients(pacientesPorDiagnostico, ulPacientes);
+
+    } else {
+        printAllpacients(pacientes, ulPacientes);
+    }
+
+
+}
+
+function filtrarPorDiagnostico(list, diagnostico) {
+    return list.filter(paciente => paciente.diagnostico === diagnostico)
 }
